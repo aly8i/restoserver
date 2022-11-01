5eee4c2cbecb70e42b9b26d819f103e47b53da50
aly8i <ABY00003@students.aust.edu.lb>
2022-11-02 01:33:17 +0200

    5 try

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5000
 require('dotenv').config();
 app.use(
   cors({
-    origin: `${process.env.CLIENT_URL}/`,
+    origin: `${process.env.CLIENT_URL}`,
     methods: "GET,POST,PUT,DELETE",
     credentials: true
   })
