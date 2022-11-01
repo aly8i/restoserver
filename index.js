50024c0e5f29cfa23817bd7734c6c822547e7712
aly8i <ABY00003@students.aust.edu.lb>
2022-11-02 01:56:06 +0200

    eijfei

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 // );
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true")
   res.header(
     "Access-Control-Allow-Headers",
